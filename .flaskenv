d7021010f950ca3a3f70b5c54c3d2089f0c5410b
御前吹水 <746722756@qq.com>
2021-07-23 16:31:04 +0800

    第一次提交，书中内容第二章之前的练习，第三章跳过；

---
+++ .flaskenv
@@ -0,0 +1,4 @@
+FLASK_ENV=development # 表明这是生产还是开发环境
+FLASK_RUN_PORT=6969  # web服务器监听的端口
+
+SECRET_KEY=random_number # 自定义的字段，用来给session设置秘钥
---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, url_for, redirect, session, request, make_response
+import os
+from jinja2.utils import generate_lorem_ipsum
+
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY', 'default secret')
+
+
+@app.route('/')
+@app.route('/hello')
+def hello():
+    name = request.args.get('name')
+    if name is None:
+        name = request.cookies.get('name', 'Human')
+        response = '<h1>Hello, %s!</h1>' % name
+        if 'logged_in' in session:
+            response += '[Authenticated]'
+        else:
+            response += '[Not Authenticated]'
+        return response
+
+
+@app.route('/login/<name>')
+def login(name):
+    if name == 'YuqianChuishui':
+        session['logged_in'] = True  # 写入session
+    res = make_response(redirect(url_for('hello')))
+    res.set_cookie('name', name)
+    return res
+
+
+@app.route('/logout')
+def logout():
+    if "logged_in" in session:
+        session.pop('logged_in')
+    return redirect(url_for('hello'))
+
+
+@app.route('/post')
+def show_post():
+    post_body = generate_lorem_ipsum(n=2)
+    return '''
+    <h1>A very long post</h1>
+    <div class="body">%s</div>
+    <button id="load">load more</button>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript">
+    $(function() {
+        $('#load').click(function() {
+            console.log('执行了点击事件')
+            $.ajax({
+                url:'/more',
+                type:'get',
+                success:function(data){
+                    $('.body').append('<p>'+data+'<p>');
+                }
+            })
+        })
+    })
+    </script>
+    ''' % post_body
+
+
+@app.route('/more')
+def load_post():
+    return generate_lorem_ipsum(n=1)
+
+
+if __name__ == '__main__':
+    app.run()
