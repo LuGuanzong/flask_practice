84962ab393ed39cd90244b93ff433f681f1baee2
御前吹水 <746722756@qq.com>
2021-09-23 05:03:38 +0800

    完成笔记基本增删改查功能

--- app.py
+++ app.py
@@ -1,70 +1,101 @@
-from flask import Flask, url_for, redirect, session, request, make_response
 import os
-from jinja2.utils import generate_lorem_ipsum
+import sys
+
+import click
+from flask import Flask
+from flask import redirect, url_for, abort, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+# SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY', 'default secret')
+app.jinja_env.trim_blocks = True  # 清除jinja模板中的空行
+app.jinja_env.lstrip_blocks = True  # 清除jinja模板中的制表符
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret string')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', prefix + os.path.join(app.root_path, 'data.db'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 是否跟踪db对象的修改，这里设为false取消相关提醒
+
+db = SQLAlchemy(app)
+
+
+@app.cli.command()
+def initdb():
+    db.create_all()
+    click.echo('Initialized database.')
+
+
+class NewNoteForm(FlaskForm):
+    body = TextAreaField('Body', validators=[DataRequired()])
+    submit = SubmitField('Save')
+
+
+class EditNoteForm(NewNoteForm):
+    submit = SubmitField('Update')
+
+
+class DeleteNoteForm(FlaskForm):
+    submit = SubmitField('Delete')
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+
+    def __rper__(self):
+        return '<Note %r>' % self.body
 
 
 @app.route('/')
-@app.route('/hello')
-def hello():
-    name = request.args.get('name')
-    if name is None:
-        name = request.cookies.get('name', 'Human')
-        response = '<h1>Hello, %s!</h1>' % name
-        if 'logged_in' in session:
-            response += '[Authenticated]'
-        else:
-            response += '[Not Authenticated]'
-        return response
+def index():
+    form = DeleteNoteForm()
+    notes = Note.query.all()
+    return render_template('index.html', notes=notes, form=form)
 
 
-@app.route('/login/<name>')
-def login(name):
-    if name == 'YuqianChuishui':
-        session['logged_in'] = True  # 写入session
-    res = make_response(redirect(url_for('hello')))
-    res.set_cookie('name', name)
-    return res
+@app.route('/new', methods=['GET', 'POST'])
+def new_note():
+    form = NewNoteForm()
+    if form.validate_on_submit():
+        body = form.body.data
+        note = Note(body=body)
+        db.session.add(note)
+        db.session.commit()
+        flash('Your note is save.')
+        return redirect(url_for('index'))
+    return render_template('new_note.html', form=form)
 
 
-@app.route('/logout')
-def logout():
-    if "logged_in" in session:
-        session.pop('logged_in')
-    return redirect(url_for('hello'))
+@app.route('/edit/<int:note_id>', methods=['GET', 'POST'])
+def edit_note(note_id):
+    form = EditNoteForm()
+    note = Note.query.get(note_id)
+    if form.validate_on_submit():
+        note.body = form.body.data
+        db.session.commit()
+        flash('Your note is updated.')
+        return redirect(url_for('index'))
+    form.body.data = note.body
+    return render_template('edit_note.html', form=form)
 
 
-@app.route('/post')
-def show_post():
-    post_body = generate_lorem_ipsum(n=2)
-    return '''
-    <h1>A very long post</h1>
-    <div class="body">%s</div>
-    <button id="load">load more</button>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script type="text/javascript">
-    $(function() {
-        $('#load').click(function() {
-            console.log('执行了点击事件')
-            $.ajax({
-                url:'/more',
-                type:'get',
-                success:function(data){
-                    $('.body').append('<p>'+data+'<p>');
-                }
-            })
-        })
-    })
-    </script>
-    ''' % post_body
-
-
-@app.route('/more')
-def load_post():
-    return generate_lorem_ipsum(n=1)
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/delete/<int:note_id>', methods=['GET', 'POST'])
+def delete_note(note_id):
+    form = DeleteNoteForm()
+    if form.validate_on_submit():
+        note = Note.query.get(note_id)
+        db.session.delete(note)
+        db.session.commit()
+        flash('Your note is deleted.')
+    else:
+        abort(400)
+    return redirect(url_for('index'))
